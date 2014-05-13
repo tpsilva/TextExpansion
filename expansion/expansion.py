7f499b8be9383380ac64645e8e1228363c83cf3c
tiagopasq <tiago.pasqualini@gmail.com>
2014-05-12 23:05:06 -0300

    refactoring for better readability

--- expansion/expansion.py
+++ expansion/expansion.py
@@ -7,56 +7,52 @@ ORIGINAL = 1
 CONCEPTS = 2
 DISAMBIGUATION = 3
 
-def should_append(original_added, token, translation):
-    return translation != token or not original_added
-
-def expand(samples, parameters, *dictionary_filenames):
+def init_dictionaries():
     dictionaries = range(4) # check if there's a better way to create the list
     dictionaries[LINGO] = dictionary.LingoDictionary()
     dictionaries[ORIGINAL] = None
     dictionaries[CONCEPTS] = dictionary.ConceptsDictionary()
     dictionaries[DISAMBIGUATION] = dictionary.DisambiguationDictionary()
 
+    return dictionaries
+
+def translate(token, dictionary, expanded_sample):
+    translation = dictionary.translate(token)
+    if translation not in expanded_sample:
+        expanded_sample.append(translation)
+
+def expand(samples, parameters, *dictionary_filenames):
+    dictionaries = init_dictionaries()
+
     custom_dictionaries = []
     for dictionary_filename in dictionary_filenames:
         custom_dictionaries.append(dictionary.GenericDictionary(dictionary_filename))
 
-    translated_samples = []
+    expanded_samples = []
     for sample in samples:
-        translated_sample = []
+        expanded_sample = []
         
         if parameters[DISAMBIGUATION]: # only init disambiguation when needed
             dictionaries[DISAMBIGUATION].init_concepts(sample)
 
-        for token in re.split("[\t\ ]", sample):
+        for token in re.split("[\t\ ]", sample.lower()):
             original_added = False
 
             if parameters[ORIGINAL]:
-                translated_sample.append(token)
-                original_added = True
+                expanded_sample.append(token)
 
             if parameters[LINGO]:
-                lingo_translation = dictionaries[LINGO].translate(token)
-                if should_append(original_added, token, lingo_translation):
-                    translated_sample.append(lingo_translation)
-                    original_added = original_added or lingo_translation == token
+                translate(token, dictionaries[LINGO], expanded_sample)
 
             if parameters[CONCEPTS] and not parameters[DISAMBIGUATION]:
-                concepts_translation = dictionaries[CONCEPTS].translate(token)
-                if should_append(original_added, token, concepts_translation):
-                    translated_sample.append(concepts_translation)
-                    original_added = original_added or lingo_translation == token
+                translate(token, dictionaries[CONCEPTS], expanded_sample)
 
             if parameters[DISAMBIGUATION]:
-                disambiguation_translation = dictionaries[DISAMBIGUATION].translate(token)
-                if should_append(original_added, token, disambiguation_translation):
-                    translated_sample.append(disambiguation_translation)
-                    original_added = original_added or lingo_translation == token
+                translate(token, dictionaries[DISAMBIGUATION], expanded_sample)
 
             for custom_dictionary in custom_dictionaries:
-                translation = custom_dictionary.translate(token)
-                if should_append(original_added, token, translation):
-                    translated_sample.append(translation)
-                    original_added = original_added or lingo_translation == token
+                translate(token, custom_dictionary, expanded_sample)
 
-        print " ".join(translated_sample)
+        expanded_samples.append(" ".join(expanded_sample))
+
+    print "\n".join(expanded_samples)
