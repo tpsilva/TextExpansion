b42fa25bcf88d2900787ef78e79650661f7e7740
tiagopasq <tiago.pasqualini@gmail.com>
2014-10-08 23:28:32 -0300

    removed babelnet installation from setup.sh

--- setup.sh
+++ setup.sh
@@ -3,12 +3,3 @@
 echo "Cloning lingotranslator from github"
 
 git clone https://github.com/tiagopasq/lingotranslator
-
-echo "Do you want to install BabelNet?"
-read install_babel
-
-if [ $install_babel = 'y' ]
-then
-    install_babelnet
-    echo "All done!"
-fi
